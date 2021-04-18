dadb7ecd9603ea0432962efaaf8b9acb9a95b880
Parad0xial <64382080+Parad0xial@users.noreply.github.com>
2021-04-17 19:29:42 -0700

    created networked player prefab

---
+++ Real Time Multiplayer/Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rb;
+    PhotonView photonView;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        photonView = GetComponent<PhotonView>();
+    }
+
+    void FixedUpdate()
+    {
+        // Only move the player object if it's the local users player
+        if (photonView.IsMine)
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        Vector2 moveVector = new Vector2(horizontal, vertical);
+
+        rb.position += moveVector;
+    }
+}
--- Real Time Multiplayer/Assets/Scripts/NetworkConnector.cs
+++ Real Time Multiplayer/Assets/Scripts/NetworkConnector.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class NewBehaviourScript : MonoBehaviour
+public class NetworkConnector : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    #region Pun Callbacks
+
+    public override void OnConnectedToMaster()
     {
-        
+        // Try to join a random room
+        PhotonNetwork.JoinRandomRoom();
     }
+
+
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        // Failed to connect to random, probably because none exist
+        Debug.Log(message);
+
+        // Create a new room to join instead
+        PhotonNetwork.CreateRoom("My First Room");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log($"{PhotonNetwork.CurrentRoom.Name} joined!");
+    }
+
+    #endregion
 }
+
